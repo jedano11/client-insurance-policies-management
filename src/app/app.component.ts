e44f98f7e962b378123e9a56889867c056efb04d
Jomar Edano <jomar@vananaz.com>
2021-01-26 12:38:49 +0800

    Navbar design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from './_services';
 import { IUser } from './_models';
 
-@Component({ selector: 'app-root', templateUrl: 'app.component.html' })
+@Component({ selector: 'app-root', templateUrl: 'app.component.html', styleUrls: ['./app.component.css'] })
 export class AppComponent implements OnInit {
   user: IUser = { id: '', name: '', email: '', role: '' };
 
